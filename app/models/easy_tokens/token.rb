3155e98f2714adccfb8f82680624c203769780d2
Adam Nowak <lubieniebieski@gmail.com>
2014-11-29 19:12:40 +0100

    assign token value after it is initialized

--- app/models/easy_tokens/token.rb
+++ app/models/easy_tokens/token.rb
@@ -1,4 +1,11 @@
 module EasyTokens
   class Token < ActiveRecord::Base
+    after_initialize :ensure_token_presence
+
+    private
+
+    def ensure_token_presence
+      self.value ||= SecureRandom.hex(16)
+    end
   end
 end
--- test/models/easy_tokens/token_test.rb
+++ test/models/easy_tokens/token_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 module EasyTokens
   class TokenTest < ActiveSupport::TestCase
+    test 'assigns value after initialization' do
+      token = Token.new
+      assert_not token.value.nil?
+    end
     # test "the truth" do
     #   assert true
     # end
