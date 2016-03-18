3f2d721eee1dd7138c6968a86dfc2624c85466e2
Marcin Jakubowski <marcin.jakubowski@netguru.pl>
2016-03-18 17:44:00 +0100

    Revert "Conditionally include EasyTokens::Authorization module in ActionController:Base"
    This reverts commit b8a756290180a1502e17cf71894a4bfcf47e0946.

--- lib/easy_tokens/engine.rb
+++ lib/easy_tokens/engine.rb
@@ -1,14 +1,11 @@
 require_relative 'authorization'
-require_relative 'grape'
 
 module EasyTokens
   class Engine < ::Rails::Engine
     isolate_namespace EasyTokens
 
     initializer 'easy_tokens.controller_extensions' do
-      if Object.const_defined?('ActionController::Base')
-        ActionController::Base.send(:include, EasyTokens::Authorization)
-      end
+      ActionController::Base.send(:include, EasyTokens::Authorization)
     end
   end
 end
