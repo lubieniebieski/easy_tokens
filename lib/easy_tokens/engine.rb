b8a756290180a1502e17cf71894a4bfcf47e0946
Marcin Jakubowski <marcin.jakubowski@netguru.pl>
2016-03-18 15:19:51 +0100

    Conditionally include EasyTokens::Authorization module in ActionController:Base
    From now gem is able to work with GrapeAPI. So it’s possible to use
    this gem without rails.

--- lib/easy_tokens/engine.rb
+++ lib/easy_tokens/engine.rb
@@ -1,11 +1,14 @@
 require_relative 'authorization'
+require_relative 'grape'
 
 module EasyTokens
   class Engine < ::Rails::Engine
     isolate_namespace EasyTokens
 
     initializer 'easy_tokens.controller_extensions' do
-      ActionController::Base.send(:include, EasyTokens::Authorization)
+      if Object.const_defined?('ActionController::Base')
+        ActionController::Base.send(:include, EasyTokens::Authorization)
+      end
     end
   end
 end
